f43267dd7241e3649f8c9a5bb3d7f04a3f52cb15
NALHOUG <Me914374385>
2021-05-23 22:37:47 +0800

    初始化Vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+  <v-container>
+    <v-app-bar app>
+      <v-btn text class="ml-6 mr-4">
+        Yarnk
+      </v-btn>
+      <v-tabs>
+        <v-tab v-for="(item,n) in navs" :key="n">
+          {{ item.name }}
+        </v-tab>
+      </v-tabs>
+    </v-app-bar>
 
+    <v-card>
+      <v-card-title>
+        文章动态
+      </v-card-title>
+
+    </v-card>
+
+    <v-footer app>
+      <div style="width: 100%" class="d-flex justify-center">
+        © 2021 YARNK BLOG
+      </div>
+    </v-footer>
+  </v-container>
+</template>
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    navs: [
+      {
+        name: '首页'
+      },
+      {
+        name: '文章'
+      },
+      {
+        name: '关于'
+      },
+      {
+        name: '更多'
+      }
+    ]
+  })
 }
 </script>
